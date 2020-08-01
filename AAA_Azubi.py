c26e79198460b4d59b4c8f12c21272bc1f399ee1
Ama Owusu-Darko <39912040+ktemadarko@users.noreply.github.com>
2020-08-01 10:55:32 +0000

    Initial code

---
+++ AAA_Azubi.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 31 17:48:07 2020
+
+@author: MPH
+"""
+
+import datetime as dt
+import pandas as pd
+
+
+done= False
+tasks_info={}
+
+
+print("Hello, Welcome to Nana's Timesheet"
+      "\nTo start type the task number eg. Task 1 for Task 1 or type done to exit."
+      "\n\nUse the 24 hour format for the start time and stop time."
+      "\nEg. Enter 11:00 for a task starting at 11am and 13:30 for a task ending at 1:30 pm.\n")
+
+while not done:
+    #Get task_number
+    task_number= input("Enter the name of the task or type done to exit: \n")
+
+    if task_number.lower()!="done":
+        tasks_info["Task "+task_number]={}
+        #Get Start_time
+        begin=input("What time did you start Task "+ task_number+ "? \n")
+        tasks_info["Task "+task_number]['Start_time']=begin
+
+        #Get Stop_time
+        end=input("What time did you end Task "+ task_number+ "? \n")
+        if begin>end:
+            end=input("Enter a time after "+begin + "GMT. \nWhat time did you end Task "+ task_number+ "? \n")
+        else:
+            tasks_info["Task "+task_number]['Stop_time']= end
+
+        #Get duration
+        start_dt = dt.datetime.strptime(begin, '%H:%M')
+        end_dt = dt.datetime.strptime(end, '%H:%M')
+        duration=end_dt - start_dt
+        difference=duration.seconds/3600
+
+
+        #Hourly_rate=float(input("How much did you earn per hour? Enter an integer or a decimal value: \n\n"))
+        
+        tasks_info["Task "+task_number]['Duration of Work (Hours)']=difference
+        tasks_info["Task "+task_number]['Money_earned (Dollars)']=round(float(5*difference))
+        
+
+    else: #end loop if done
+        #del tasks_info["Taskdone"]
+        #tasks_info
+
+        tasks=pd.DataFrame.from_dict(tasks_info)
+        Timesheet=tasks.T#transpose dataframe
+        
+        print("Since Nana earns 5 dollars an hour. Here is his Timesheet.\n")
+        print(Timesheet)
+        Timesheet.to_excel("Azubi_Python_output"+str(len(Timesheet))+".xlsx", engine="xlsxwriter")
+        print("\nThat is it, check on your computer for a document called \nAzubi_Python_output"+str(len(Timesheet))+".xlsx" 
+              "\nfor your Timetable in a Microsoft Excel file")
+       
+        done=True
